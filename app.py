a4b5f1ffa7951ea86168bd5f8c4cab9bdb36ca42
Rafael Ortega <123382297+rafaelinopro@users.noreply.github.com>
2023-07-07 20:47:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+# Route for the GitHub webhook
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./pythonanywhere')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
+
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Mamá mi primera aplicación</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
